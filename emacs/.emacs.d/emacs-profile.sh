edc384ec282a58ed5ace663efac662c75c44c3c2
eccentricayman <aahmed3@stuy.edu>
2016-12-28 22:42:05 -0500

    added terminal emacsprofile option

--- emacs/.emacs.d/emacs-profile.sh
+++ emacs/.emacs.d/emacs-profile.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-echo "Opening .emacs profiler..."
-emacs -Q -l ~/.emacs.d/lisp/profile-dotemacs/profile-dotemacs.el -f profile-dotemacs &
+echo -e "1)GUI\n2)Terminal\nPlease choose an Emacs to profile."
+read type
+
+if [[ $type -eq 1 ]]
+then
+    echo "Opening .emacs profiler for GUI..."
+    emacs -Q -l ~/.emacs.d/lisp/profile-dotemacs/profile-dotemacs.el -f profile-dotemacs &
+elif [[ $type -eq 2 ]]
+then
+    echo "Opening .emacs profiler for terminal..."
+    emacs -Q -nw -l ~/.emacs.d/lisp/profile-dotemacs/profile-dotemacs.el -f profile-dotemacs
+else
+    echo "Please enter 1 or 2."
+fi
