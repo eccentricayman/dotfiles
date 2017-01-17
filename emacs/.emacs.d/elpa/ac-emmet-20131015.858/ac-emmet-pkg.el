4ec26e24f20c867a750324749efc5a8b54e9c5d5
eccentricayman <aahmed3@stuy.edu>
2017-01-16 20:17:15 -0500

    removed some packages, fixed html completion with emmet

--- emacs/.emacs.d/elpa/ac-emmet-20131015.858/ac-emmet-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ac-emmet" "20131015.858" "auto-complete sources for emmet-mode's snippets" '((emmet-mode "1.0.2") (auto-complete "1.4")) :url "https://github.com/yasuyk/ac-emmet" :keywords '("completion" "convenience" "emmet"))
